0bcee998f19a65c8d3eb26d42c96ab9c8066f09c
Skye Dy <kevinsky@acale.ph>
2013-10-01 12:01:51 +0800

    added UptimeCollector

---
+++ src/collectors/uptime/uptime.py
@@ -0,0 +1,24 @@
+import diamond.collector
+
+class UptimeCollector(diamond.collector.Collector):
+    def get_default_config(self):
+        """
+        Returns the default collector settings
+        """
+        config = super(UptimeCollector, self).get_default_config()
+        config.update({
+            'path':     'uptime'
+        })
+
+        return config
+
+    def collect(self):
+        PROC = '/proc/uptime'
+
+        with open(PROC, 'r+b') as procFile:
+            line = procFile.readline()
+            uptime, idletime = [ float(val) for val in line.strip().split(" ") ]
+
+            self.publish('up', uptime)
+            self.publish('idle', idletime)
+
