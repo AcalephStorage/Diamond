3252b998c17db6efeb7390fe03f8849725c0ee83
Skye Dy <kevinsky@acale.ph>
2014-02-23 18:05:18 +0800

    ACA-101 #comment removed unused config param in cephcollector, created initial cephpoolstatscollector

---
+++ src/collectors/cephpoolstats/cephpoolstats.py
@@ -0,0 +1,44 @@
+import subprocess
+import re
+import os
+import sys
+import shlex
+import json
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, '../ceph')
+
+import ceph
+
+
+"""
+Get ceph pool stats from one node
+"""
+
+
+class CephPoolStatsCollector(ceph.CephCollector):
+    def _get_stats(self):
+        """
+        Get ceph stats
+        """
+        cmd = "%s --cluster=%s osd pool stats --format=json" % (self.config['ceph_binary'], self.config['cluster_name'])
+        cmd_spl = shlex.split(cmd)
+
+        try:
+            output = subprocess.check_output(cmd_spl)
+        except subprocess.CalledProcessError, err:
+            self.log.info(
+                'Could not get pool stats: %s' % err)
+            self.log.exception('Could not get pool stats')
+            return {}
+
+        return json.loads(output)
+
+    def collect(self):
+        """
+        Collect ceph stats
+        """
+        stats = self._get_stats()
+        self._publish_stats('cephpoolstats', stats)
+
+        return
