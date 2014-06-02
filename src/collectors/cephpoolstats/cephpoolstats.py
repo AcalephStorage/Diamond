77518feed92d95e1608cde3cca2cdc8b7ccdb68e
Skye Dy <kevinsky@acale.ph>
2014-06-02 15:47:05 +0800

    Updated CephPoolStatsCollector due to merge with Calamari.

--- src/collectors/cephpoolstats/cephpoolstats.py
+++ src/collectors/cephpoolstats/cephpoolstats.py
@@ -5,6 +5,8 @@ import sys
 import shlex
 import json
 
+import diamond.collector
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, '../ceph')
 
@@ -16,7 +18,33 @@ Get ceph pool stats from one node
 """
 
 
-class CephPoolStatsCollector(ceph.CephCollector):
+class CephPoolStatsCollector(diamond.collector.Collector):
+    def get_default_config_help(self):
+        config_help = super(ceph.CephPoolStatsCollector, self).get_default_config_help()
+        config_help.update({
+            'socket_path': 'The location of the ceph monitoring sockets.'
+                           ' Defaults to "/var/run/ceph"',
+            'socket_ext': 'Extension for socket filenames.'
+                          ' Defaults to "asok"',
+            'ceph_binary': 'Path to "ceph" executable. '
+                           'Defaults to /usr/bin/ceph.',
+            'cluster_name': "Name of cluster.  Defaults to 'ceph'."
+        })
+        return config_help
+
+    def get_default_config(self):
+        """
+        Returns the default collector settings
+        """
+        config = super(CephPoolStatsCollector, self).get_default_config()
+        config.update({
+            'socket_path': '/var/run/ceph',
+            'socket_ext': 'asok',
+            'ceph_binary': '/usr/bin/ceph',
+            'cluster_name': 'ceph'
+        })
+        return config
+
     def _get_stats(self):
         """
         Get ceph pool stats
@@ -45,6 +73,18 @@ class CephPoolStatsCollector(ceph.CephCollector):
 
         self._publish_stats(self.config['cluster_name'], sums)
 
+    def _publish_stats(self, prefix, stats):
+        """
+        Given a stats dictionary, publish under the cluster path (respecting
+        short_names and cluster_prefix)
+        """
+
+        for stat_name, stat_value in ceph.flatten_dictionary(
+            stats,
+            path=[prefix]
+        ):
+            self.publish_gauge(name, stat_value)
+
     def collect(self):
         """
         Collect ceph pool stats
@@ -56,7 +96,7 @@ class CephPoolStatsCollector(ceph.CephCollector):
             pool_id = pool.pop('pool_id')
             pool_name = pool.pop('pool_name')
             
-            self._publish_stats('ceph.%s' % (cluster_name, pool_name), pool)
+            self._publish_stats('%s.%d' % (self.config['cluster_name'], pool_id), pool)
 
         self._publish_stat_sums(stats)
 
